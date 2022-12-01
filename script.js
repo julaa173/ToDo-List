767d3aa57d89e8a05e84a8675c7d3d8e7a1341d1
olivera_julieta@outlook.com <olivera_julieta@outlook.com>
2022-12-01 15:50:41 -0300

    Clase 05 completada

--- script.js
+++ script.js
@@ -3,28 +3,42 @@ import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector('[data-form-btn]');
 
+
+const addTask = (evento) => {
+    const list = document.querySelector('[data-list]');
+    const task = createTask(evento);
+    list.appendChild(task);
+}
+
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
+    const calendar = document.querySelector('[data-form-date]');
     const value = input.value;
-    const list = document.querySelector('[data-list]')
+    const date = calendar.value;
+    const dateFormat = moment(date).format('DD/MM/YYYY'); 
+    
     const task = document.createElement('li');
     task.classList.add('card');
     input.value = '';
 
     const taskContent = document.createElement('div');
+
     const titleTask = document.createElement('span');
     titleTask.classList.add('task');
     titleTask.innerHTML = value;
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-
-    // task.innerHTML = content;  
+    // task.innerHTML = content; 
+    const dateElement = document.createElement('span');
+    dateElement.innerHTML = dateFormat;
     task.appendChild(taskContent);
+    task.appendChild(dateElement);
     task.appendChild(deleteIcon());
-    list.appendChild(task);
+    
+    return task
 
     };
 
-    btn.addEventListener("click", createTask);
+    btn.addEventListener("click", addTask);
 
